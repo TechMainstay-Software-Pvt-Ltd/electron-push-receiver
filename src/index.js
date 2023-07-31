bdc74a88c3be14a45eed761036f6784071d1586a
sid-tmbill <sid@tmbill.com>
2023-07-31 10:26:07 +0530

    chore: minor bug fixes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { register, listen } = require('@tmbill/push-receiver');
+const { PushReceiver } = require('@tmbill/push-receiver');
 const { ipcMain } = require('electron');
 const Config = require('electron-config');
 const {
@@ -11,25 +11,16 @@ const {
 
 const config = new Config();
 
-module.exports = {
-  START_NOTIFICATION_SERVICE,
-  NOTIFICATION_SERVICE_STARTED,
-  NOTIFICATION_SERVICE_ERROR,
-  NOTIFICATION_RECEIVED,
-  TOKEN_UPDATED,
-  setup,
-  reset,
-};
-
 // To be sure that start is called only once
 let started = false;
+let pushReceiverinstance;
 
 // To be call from the main process
 function setup(webContents) {
   // Will be called by the renderer process
   ipcMain.on(START_NOTIFICATION_SERVICE, async (_, senderId) => {
     // Retrieve saved credentials
-    let credentials = config.get('credentials');
+    const credentials = config.get('credentials');
     // Retrieve saved senderId
     const savedSenderId = config.get('senderId');
     if (started) {
@@ -40,24 +31,44 @@ function setup(webContents) {
       return;
     }
     started = true;
+
     try {
       // Retrieve saved persistentId : avoid receiving all already received notifications on start
       const persistentIds = config.get('persistentIds') || [];
       // Register if no credentials or if senderId has changed
       if (!credentials || savedSenderId !== senderId) {
-        credentials = await register(senderId);
-        // Save credentials for later use
-        config.set('credentials', credentials);
+        pushReceiverinstance = new PushReceiver({
+          senderId,
+          persistentIds,
+          credentials,
+        });
+
+        pushReceiverinstance.onCredentialsChanged(({ newCredentials }) => {
+          console.log(
+            'Client generated new credentials. Save them somewhere! And decide if thing are needed to re-subscribe',
+            newCredentials,
+          );
+          // Save credentials for later use
+          config.set('credentials', newCredentials);
+        });
+
+        pushReceiverinstance.onNotification((notification) => {
+          // Do someting with the notification
+          const savedpersistentIds = config.get('persistentIds') || [];
+          // Update persistentId
+          config.set('persistentIds', [...persistentIds, savedpersistentIds]);
+          // Notify the renderer process that a new notification has been received
+          // And check if window is not destroyed for darwin Apps
+          if (!webContents.isDestroyed()) {
+            webContents.send(NOTIFICATION_RECEIVED, notification);
+          }
+        });
+
         // Save senderId
         config.set('senderId', senderId);
         // Notify the renderer process that the FCM token has changed
         webContents.send(TOKEN_UPDATED, credentials.fcm.token);
       }
-      // Listen for GCM/FCM notifications
-      await listen(
-        { ...credentials, persistentIds },
-        onNotification(webContents),
-      );
       // Notify the renderer process that we are listening for notifications
       webContents.send(NOTIFICATION_SERVICE_STARTED, credentials.fcm.token);
     } catch (e) {
@@ -73,17 +84,18 @@ function reset() {
   config.set('senderId', null);
   config.set('persistentIds', null);
   started = false;
+  if (pushReceiverinstance) {
+    pushReceiverinstance.destroy();
+    pushReceiverinstance = undefined;
+  }
 }
-// Will be called on new notification
-function onNotification(webContents) {
-  return ({ notification, persistentId }) => {
-    const persistentIds = config.get('persistentIds') || [];
-    // Update persistentId
-    config.set('persistentIds', [...persistentIds, persistentId]);
-    // Notify the renderer process that a new notification has been received
-    // And check if window is not destroyed for darwin Apps
-    if (!webContents.isDestroyed()) {
-      webContents.send(NOTIFICATION_RECEIVED, notification);
-    }
-  };
-}
+
+module.exports = {
+  START_NOTIFICATION_SERVICE,
+  NOTIFICATION_SERVICE_STARTED,
+  NOTIFICATION_SERVICE_ERROR,
+  NOTIFICATION_RECEIVED,
+  TOKEN_UPDATED,
+  setup,
+  reset,
+};
